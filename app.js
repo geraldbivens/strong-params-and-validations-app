dc40ca1ac8a81c96030ca4bc42edc492e58da3f9
Gerald Bivens <geraldbivens@gmail.com>
2021-03-16 16:32:13 -0600

    Check that files are connected, and fetch users from the backend.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Hi there!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("%cDOM Content Loaded and Parsed!", "color: magenta");
+
+  function getExistingUserData() {
+    fetch("http://localhost:3000/users")
+      .then((response) => response.json())
+      .then((result) => console.log("Users:", result));
+    //   .then(console.log); This is the functional programming equivalent of the preceding line
+  }
+
+  getExistingUserData();
+});
